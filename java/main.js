1ca2e7eebf46e3332090c198bc5f869c79fc6011
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-05-16 20:47:58 -0300

    proximo comit

--- java/main.js
+++ java/main.js
@@ -1,6 +1,16 @@
+
+let logo = document.querySelector('#logo')
+let menu = document.querySelector('.menu')
+
 function onScroll(){
 
     if(scrollY > 0){
         navigation.classList.add('scroll')
+        logo.src = "assets/logo_nova.svg"
+        menu.src = "assets/menu_nova.svg"
+    }else{
+        navigation.classList.remove('scroll')
+        logo.src = "assets/logo.svg"
+        menu.src = "assets/menu.svg"
     }
 }
