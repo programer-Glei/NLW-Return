f4755a551f87ce843e361e90f68f7b672c7c47eb
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-20 11:22:35 -0300

    proximo comit

--- java/main.js
+++ java/main.js
@@ -23,3 +23,9 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal('#home, #home img, #home .stats')
